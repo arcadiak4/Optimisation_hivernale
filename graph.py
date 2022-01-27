d0c65b82c6a74e75fef07eae0ad00d645b1136c0
Celia RESTES <celia.restes@epita.fr>
2022-01-27 14:34:58 +0100

    maj graph.py

--- graph.py
+++ graph.py
@@ -1,7 +1,6 @@
 import pprint
 from collections import defaultdict
 
-
 class Graph(object):
     """ Graph data structure, undirected by default. """
 
@@ -22,7 +21,16 @@ class Graph(object):
         self.Graph[node2].append((node1, weight))
 
     def removeNode(self, node):
-      pass
+      
+      #Suppretion node
+      if node in self.Graph:
+        del self.Graph[node]
+
+      # Suppression des intersections
+      for _node in self.Graph:
+        for _next in self.Graph[_node]:
+          if _next[0] == node:
+            self.Graph[_node].remove(_next)
     
     def __str__(self):
         str_return = ""
@@ -30,22 +38,6 @@ class Graph(object):
             str_return += node + " : " + str(self.Graph[node]) + '\n'
         return str_return
     
-# Test
-list_node_edge = {
-    "A" : [("H",6),("B",12),("C",6),("D",10),("G",8)],
-    "B" : [("H",6),("I",2),("A",12),("C",6)],
-    "C" : [("B",6),("I",3),("A",6),("D",1)],
-    "D" : [("A",10),("C",1),("E",2),("F",4)],
-    "F" : [("D",4),("G",2)],
-    "G" : [("A",8),("F",2)],
-    "H" : [("A",6),("B",6)],
-    "I" : [("B",2),("C",3)]
-    }
-
-graph = Graph(list_node_edge, True)
-graph.addNode("L", "J", 5)
-print(graph)
-
     
 
 
