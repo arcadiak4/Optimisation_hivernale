77ff6898f4cac8c8d81574b2c56be6b38dd9a358
Basma <basma.bouharicha@epita.fr>
2022-02-05 21:20:28 +0100

    debut djikstra

--- graph.py
+++ graph.py
@@ -29,7 +29,64 @@ class Graph(object):
         for node in self.Graph:
             str_return += node + " : " + str(self.Graph[node]) + '\n'
         return str_return
-    
+
+        
+###DJIKSTRA incomplet malheuresement...
+   #pour meux comprendre ce que j'ai tenté de faire : 
+   #"https://fr.wikipedia.org/wiki/Algorithme_de_Dijkstra#Impl%C3%A9mentation_de_l'algorithme"
+       
+    def get_nodes(self):#return a list of all the nodes
+        list_nodes=[]
+        for _node in self.Graph:
+            list_nodes.append(_node)
+        return list_nodes     
+            
+    def initalization(self, node):
+        #on initialise les sommets à infini(= sys.maxsize)
+        for _node in self.Graph:
+            for _next in self.Graph[_node]:
+                lnext = list(_next)
+                lnext[1] = sys.maxsize   
+                _next = tuple(lnext)
+        #from node to node -> dist = 0        
+        #self.Graph[node].append((node,0))
+        
+        
+    def find_min(self, node): #retourne le sommet avec le poids minimum -> le plus petit des poids(node,sommet) 
+        mini = sys.maxsize
+        n = None
+        for t in self.Graph[node]:
+            if t[1] < mini:
+                mini = t[1]
+                n = t[0]
+       
+        return n
+
+    def maj_dist(self,node,node1,node2):
+        for t in self.Graph[node1]:
+            if t[0] == node2:
+                d12=t[1] #poids(node,node1)
+        for t in self.Graph[node]:
+            if t[0] == node1 :
+                d1=t[1] #poids(node,node1)
+            if t[0] == node2:
+                d2= t[1] #poids(node,node2)
+
+        #if d2 > (d1 + d12) :
+        #  predecesseur[node2]=node1
+            
+   
+    def djikstra(self, node):    
+        #initializatoin(self,node)
+        list_nodes = self.Graph.get_nodes()
+        list_nodes.remove(node)
+        #tant que liste n'est pas vide
+        #while len(list_nodes) > 0:
+            
+
+
+
+
 # Test
 list_node_edge = {
     "A" : [("H",6),("B",12),("C",6),("D",10),("G",8)],
